54e209d712f50cbc474bc46adcb86b8ee3de5281
lindsay <hoarau.lly@gmail.com>
2026-03-27 07:49:03 +0000

    fin de l'exercice 10

--- build/messStacker.c
+++ build/messStacker.c
@@ -7,6 +7,21 @@
 #include <string.h>
 /* ========================================================================== */
 
+//Constante 
+
+#define SIZE_STACK 30;
+
+/* ========================  Variables globales  ========================= */
+
+MESSAGE stackMess[SIZE_STACK]; //tableau (comme tableau circulaire)
+int lastPos = -1;              // position du dernier message 
+int curPos = 0;                // posiiton du message courant dans la file 
+int messCount = 0;             // nombre de message présent dans la file 
+
+/* ========================================================================== */
+
+// Fonction 
+
 const char * getMessStackerVersion(){
     return MESS_STAKER_VERSION;
 }
