fc44568399d13b8f7ab0a8dccf29dd92af580d84
lindsay <hoarau.lly@gmail.com>
2026-03-27 07:38:44 +0000

    fin de l'exercice 09

--- build/messStacker.h
+++ build/messStacker.h
@@ -1,12 +1,17 @@
 #ifndef MESSSTACKER_H
 #define MESSTACKER
 
-
-
 #define MESS_STAKER_VERSION "v0.0.01"
 #define SIZE_MAX_DATA 57
 
-// Fonction
+#include <stdbool.h>
+
+/****************************************************************************** 
+ * API de la librairie messStacker.h
+/***************************************************************************** */
+
+
+
 const char * getMessStackerVersion();
 
 typedef struct message{
@@ -16,6 +21,24 @@ typedef struct message{
     uint8_t size;                 // entier non signés sur 8 bits - nombre d'octets à considérer pour le contenu du tableau data 
     uint8_t checksum;             // checksum (8 bits)
 } MESSAGE;
+
+
+
+ /* =============================== Exercice 09 =================================== */
+
+bool sendMessage (uint8_t cmd, uint8_t size, char * data);
+
+bool haveMessage();
+bool nextMessage();
+
+uint8_t curMessageCmd();
+uint8_t curMessageSize();
+uint8_t curMessageChecksum(); 
+
+bool curMessageData(char* buffer, int lengthMax)
+
+uint8_t checksumMessage(uint8_t cmd, uint8_t size, char * data)
+
 
 #endif 
 
